8070af2148b4f8f2094b47582094c0ff134f71c8
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-31 17:12:59 +0200

    Divide App into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
