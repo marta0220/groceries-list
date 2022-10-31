5b65cc33ce2a3250593160fd8eaa8d074c6472af
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-31 20:15:48 +0200

    Make list of products

--- src/Content.js
+++ src/Content.js
@@ -1,3 +1,23 @@
+import { useState } from "react";
 export default function Content() {
-  return null;
+  const [products, setProducts] = useState([
+    { id: 1, name: "bread", checked: false },
+    { id: 2, name: "milk", checked: false },
+    { id: 3, name: "eggs", checked: false },
+  ]);
+  return (
+    <main className="container">
+      <ul>
+        {products.map((product) => (
+          <li className="product row" key={product.id}>
+            <input type="checkbox" checked={product.checked} className="col" />
+            <label className="col">{product.name}</label>{" "}
+            <button className="btn col">
+              <i class="fa-solid fa-trash-can trash"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
