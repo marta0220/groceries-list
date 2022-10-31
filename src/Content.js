b5ef94e664d1c296f4b27280c58e6b05425cbc1c
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-31 23:38:49 +0200

    Make function of deleting products

--- src/Content.js
+++ src/Content.js
@@ -12,34 +12,46 @@ export default function Content() {
     setProducts(listProducts);
     localStorage.setProduct("shoppinglist", JSON.stringify(listProducts));
   }
+  function handleDelete(id) {
+    const listProducts = products.filter((product) => product.id !== id);
+    setProducts(listProducts);
+    localStorage.setProduct("shoppinglist", JSON.stringify(listProducts));
+  }
   return (
     <main className="container">
-      <ul>
-        {products.map((product) => (
-          <li className="product row" key={product.id}>
-            <input
-              onChange={() => handleCheck(product.id)}
-              type="checkbox"
-              checked={product.checked}
-              className="col"
-            />
-            <label
-              onDoubleClick={() => handleCheck(product.id)}
-              style={
-                product.checked === true
-                  ? { textDecoration: "line-through" }
-                  : null
-              }
-              className="col"
-            >
-              {product.name}
-            </label>{" "}
-            <button className="btn col">
-              <i class="fa-solid fa-trash-can trash"></i>
-            </button>
-          </li>
-        ))}
-      </ul>
+      {products.length ? (
+        <ul>
+          {products.map((product) => (
+            <li className="product row" key={product.id}>
+              <input
+                onChange={() => handleCheck(product.id)}
+                type="checkbox"
+                checked={product.checked}
+                className="col"
+              />
+              <label
+                onDoubleClick={() => handleCheck(product.id)}
+                style={
+                  product.checked === true
+                    ? { textDecoration: "line-through" }
+                    : null
+                }
+                className="col"
+              >
+                {product.name}
+              </label>{" "}
+              <button
+                onClick={() => handleDelete(product.id)}
+                className="btn col"
+              >
+                <i class="fa-solid fa-trash-can trash"></i>
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div style={{ marginTop: "15%" }}>Your list is empty</div>
+      )}
     </main>
   );
 }
