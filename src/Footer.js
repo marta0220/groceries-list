5083306a0a74b6c0a1af533bd2bf11989cab308e
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-31 18:15:51 +0200

    Make footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,14 @@
 export default function Footer() {
-  return null;
+  return (
+    <footer>
+      <a
+        href="https://github.com/marta0220/groceries-list"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Open-source link to GitHub <i class="fa-brands fa-github"></i>
+      </a>
+    </footer>
+  );
 }
