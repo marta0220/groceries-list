cf0d82a12ed6cdf41d4f70f174dd6edf07bb72f8
Marta Pauk <martha_pauk0220@yahoo.com>
2022-10-31 21:25:11 +0200

    Make checking work

--- src/Content.js
+++ src/Content.js
@@ -5,12 +5,24 @@ export default function Content() {
     { id: 2, name: "milk", checked: false },
     { id: 3, name: "eggs", checked: false },
   ]);
+  function handleCheck(id) {
+    const listProducts = products.map((product) =>
+      product.id === id ? { ...product, checked: !product.checked } : product
+    );
+    setProducts(listProducts);
+    localStorage.setProduct("shoppinglist", JSON.stringify(listProducts));
+  }
   return (
     <main className="container">
       <ul>
         {products.map((product) => (
           <li className="product row" key={product.id}>
-            <input type="checkbox" checked={product.checked} className="col" />
+            <input
+              onChange={() => handleCheck(product.id)}
+              type="checkbox"
+              checked={product.checked}
+              className="col"
+            />
             <label className="col">{product.name}</label>{" "}
             <button className="btn col">
               <i class="fa-solid fa-trash-can trash"></i>
