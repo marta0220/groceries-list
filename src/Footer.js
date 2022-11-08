6a918a8dd9764111450003e55f8fbc908e92741e
Marta Pauk <martha_pauk0220@yahoo.com>
2022-11-08 19:41:13 +0200

    Change footer text in case of one-list product

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({ length }) {
   return (
     <footer>
-      {length} List Items
+      {length} List {length === 1 ? "item" : "items"}
       <a
         href="https://github.com/marta0220/groceries-list"
         target="_blank"
